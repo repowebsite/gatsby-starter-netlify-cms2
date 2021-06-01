ca7eb3d1838da15bb1db1a3067d7c72e88e9698c
repowebsite <rafaelfleitas18@gmail.com>
2021-06-01 08:48:51 -0300

    Update product-page.js

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -18,7 +18,7 @@ export const ProductPageTemplate = ({
   fullImage,
   pricing,
 }) => (
-  <p>aaaaaaaaaaaaaaaaa</p>
+  <p>aaaaaaaaaaaaaaaaaas</p>
   <div className="content">
     <div
       className="full-width-image-container margin-top-0"
